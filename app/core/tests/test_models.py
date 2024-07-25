c36e9a005f59628bceafe782794408863a825ba2
PavitraKailash <pavitra11297@gmail.com>
2024-07-25 19:25:18 +0530

    flake8 checks

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,7 +3,8 @@ Test Models
 """
 
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model # noqa
+
 
 class ModelTests(TestCase):
     """Test models"""
